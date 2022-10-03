e6c7b56c3ba0de03aa61ae642ffc22a5c77fffaf
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-03 21:59:43 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'ESX Speedcamera'
 
-version '1.1'
-
 server_scripts {
   '@es_extended/locale.lua',
   'locales/en.lua',
